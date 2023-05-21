177aa64313f61c6cb45213ea50ab245ac5d0d94b
pikazerochu <133658232+pikazerochu@users.noreply.github.com>
2023-05-21 15:42:32 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,122 @@
+document.getElementById("apluslogo").addEventListener('click',function(){
+    window.location.assign('index.html');
+});
+
+var about_aplus = document.getElementById("about_aplus")
+about_aplus.addEventListener('click',function(){
+    window.location.assign('index.html');
+});
+about_aplus.addEventListener('mouseover',function(){
+    about_aplus.style.color = "#efdd8d"
+});
+about_aplus.addEventListener('mouseout',function(){
+    about_aplus.style.color = "white"
+});
+
+var contact_us = document.getElementById("contact_us")
+contact_us.addEventListener('click',function(){
+    window.location.assign('apply.html')
+});
+contact_us.addEventListener('mouseover',function(){
+    contact_us.style.color = "#efdd8d"
+});
+contact_us.addEventListener('mouseout',function(){
+    contact_us.style.color = "white"
+});
+
+var aplus_training =  document.getElementById("aplus_training")
+aplus_training.addEventListener('mouseover',function(){
+    aplus_training_dropmeau.style.display = "block"
+});
+aplus_training.addEventListener('mouseout',function(){
+    aplus_training_dropmeau.style.display = "none"
+});
+
+var aplus_training_dropmeau = document.getElementById("aplus_training_dropmeau")
+aplus_training_dropmeau.addEventListener('mouseover',function(){
+    aplus_training_dropmeau.style.display = "block"
+});
+aplus_training_dropmeau.addEventListener('mouseout',function(){
+    aplus_training_dropmeau.style.display = "none"
+});
+
+var aplus_tools =  document.getElementById("aplus_tools")
+aplus_tools.addEventListener('mouseover',function(){
+    aplus_tools_dropmeau.style.display = "block"
+});
+aplus_tools.addEventListener('mouseout',function(){
+    aplus_tools_dropmeau.style.display = "none"
+});
+var aplus_tools_dropmeau = document.getElementById("aplus_tools_dropmeau")
+aplus_tools_dropmeau.addEventListener('mouseover',function(){
+    aplus_tools_dropmeau.style.display = "block"
+});
+aplus_tools_dropmeau.addEventListener('mouseout',function(){
+    aplus_tools_dropmeau.style.display = "none"
+});
+
+var leadership = document.getElementById("leadership")
+leadership.addEventListener('click',function(){
+    window.location.assign('leadership.html')
+});
+leadership.addEventListener('mouseover',function(){
+    leadership.style.color = "#efdd8d"
+});
+leadership.addEventListener('mouseout',function(){
+    leadership.style.color = "white"
+});
+
+var enlightenment = document.getElementById("enlightenment")
+enlightenment.addEventListener('click',function(){
+    window.location.assign('enlightenment.html')
+});
+enlightenment.addEventListener('mouseover',function(){
+    enlightenment.style.color = "#efdd8d"
+});
+enlightenment.addEventListener('mouseout',function(){
+    enlightenment.style.color = "white"
+});
+
+var preserverance = document.getElementById("preserverance")
+preserverance.addEventListener('click',function(){
+    window.location.assign('preserverance.html')
+});
+preserverance.addEventListener('mouseover',function(){
+    preserverance.style.color = "#efdd8d"
+});
+preserverance.addEventListener('mouseout',function(){
+    preserverance.style.color = "white"
+});
+
+var liberty = document.getElementById("liberty")
+liberty.addEventListener('click',function(){
+    window.location.assign('liberty.html')
+});
+liberty.addEventListener('mouseover',function(){
+    liberty.style.color = "#efdd8d"
+});
+liberty.addEventListener('mouseout',function(){
+    liberty.style.color = "white"
+});
+
+var influentialwords = document.getElementById("influentialwords")
+influentialwords.addEventListener('click',function(){
+    window.location.assign('influentialwords.html')
+});
+influentialwords.addEventListener('mouseover',function(){
+    influentialwords.style.color = "#efdd8d"
+});
+influentialwords.addEventListener('mouseout',function(){
+    influentialwords.style.color = "white"
+});
+
+var ateamlogos = document.getElementById("ateamlogos")
+ateamlogos.addEventListener('click',function(){
+    window.location.assign('ateamlogos.html')
+});
+ateamlogos.addEventListener('mouseover',function(){
+    ateamlogos.style.color = "#efdd8d"
+});
+ateamlogos.addEventListener('mouseout',function(){
+    ateamlogos.style.color = "white"
+});
